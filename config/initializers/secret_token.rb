f24ec4211acffe79d6447a6ceb597fd5231e504d
Robert Martin <robertmartinbc@shaw.ca>
2013-10-03 10:31:47 -0700

    First commit and README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letemlope::Application.config.secret_token = '209f66cc4c0bf998e76d8cdf866e1564b331b0c9858bf6c77716034eb8b85c302bf9fd397cc8335d45078dc55184aa6213a0225f425503d99ad305f387ebc033'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Letemlope::Application.config.session_store :cookie_store, key: '_letemlope_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Letemlope::Application.config.session_store :active_record_store
